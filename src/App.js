d7270e0ee01e5ab3de4a005c802edac69d423b65
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2025-01-16 09:52:26 +0300

    fix 12

--- src/App.js
+++ src/App.js
@@ -92,8 +92,8 @@ export default function AppWithProviders() {
 
 	useEffect(()=>{
 		if (iOS()) {
-			document.documentElement.style.setProperty('--ios-padding-top', `${30}px`);
-			document.documentElement.style.setProperty('--ios-padding-bottom', `${0}px`);
+			document.documentElement.style.setProperty('--ios-padding-top', `${0}px`);
+			document.documentElement.style.setProperty('--ios-padding-bottom', `${30}px`);
 		} else {
 			document.documentElement.style.setProperty('--ios-padding-top', `${0}px`);
 			document.documentElement.style.setProperty('--ios-padding-bottom', `${0}px`);
